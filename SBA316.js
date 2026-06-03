d54c3ad8b2b14d937c0901c3fe80eee6ce665094
Sammy65433 <samiamlistening@gmail.com>
2026-06-02 22:48:56 -0400

    SBA 316: The Document Object Model update 10

--- SBA316.js
+++ SBA316.js
@@ -6,35 +6,33 @@
 // The app gives a clue (e.g., “This team plays in Los Angeles and wears purple and gold.”).
 // The player types the team name into a form field and submits it.
 
-
-
 // 1. build html first
 
 // 2. keep it small - 5 rounds
 // Game Data
 // Put teams in array list - obj has 2props clue/answer
 
-const teams = [{
-        clue: "Founded in 1947, this franchise won its first championship in 1972.",
-        answer: "lakers",
-    },
-    {
-        clue: "This team has the most NBA championships.",
-        answer: "celtics",
-    },
-    {
-        clue: "This team dominated the 1990s, led by a player nicknamed “His Airness” who wore number 23.",
-        answer: "bulls"
-    },
-    {
-        clue: "This team set single-season record with 73 wins in 2016",
-        answer: "warriors",
-    },
-    {
-        clue: "This team won back-to-back titles in 2012-13",
-        answer: "Heat",
-    },
-
+const teams = [
+  {
+    clue: "Founded in 1947, this franchise won its first championship in 1972.",
+    answer: "lakers",
+  },
+  {
+    clue: "This team has the most NBA championships.",
+    answer: "celtics",
+  },
+  {
+    clue: "This team dominated the 1990s, led by a player nicknamed “His Airness” who wore number 23.",
+    answer: "bulls",
+  },
+  {
+    clue: "This team set single-season record with 73 wins in 2016",
+    answer: "warriors",
+  },
+  {
+    clue: "This team won back-to-back titles in 2012-13",
+    answer: "Heat",
+  },
 ];
 
 // console.log(teams);
@@ -62,147 +60,150 @@ const historyEl = document.getElementById("history");
 
 // console.log(clueEl);
 
-// Game logic 
-// Show a clue - get a guess - give feedback - show next clue 
+// Game logic
+// Show a clue - get a guess - give feedback - show next clue
 
 // / flow summary -
 // page load -> rendeClue() user see clue
-//           
+//
 
 // render 1st clue
 // Pull clue str from team
 function renderClue() {
-    //   console.log("renderClue called - index:", currentIndex);
-    //   console.log("clue text:", teams[currentIndex].clue);
+  //   console.log("renderClue called - index:", currentIndex);
+  //   console.log("clue text:", teams[currentIndex].clue);
 
-    // writing text in element <p id="clue"
-    //   without this text, player has no prompt, - Meaningless
-
-    
-    clueEl.textContent = teams[currentIndex].clue;
+  // writing text in element <p id="clue"
+  //   without this text, player has no prompt, - Meaningless
+  clueEl.textContent = teams[currentIndex].clue;
 }
 renderClue();
 
 // player sees clue (renderClue)
-// they type a guess and click submit ----submit form fires 
-// handleGuess takes over and make sure its not blank 
-// when it passes move on checkAnswer 
+// they type a guess and click submit ----submit form fires
+// handleGuess takes over and make sure its not blank
+// when it passes move on checkAnswer
 
-// user submits form -> handleGuess() (validation) -> checkAnswer 
-// Call when page loads 
+// user submits form -> handleGuess() (validation) -> checkAnswer
+// Call when page loads
 
-// Event listener 
-formEl.addEventListener('submit', handleGuess);
-resetBtn.addEventListener('click', resetGame);
+// Event listener
+formEl.addEventListener("submit", handleGuess);
+resetBtn.addEventListener("click", resetGame);
 
 // Handle Guess
 function handleGuess(e) {
-    // console.assert.log('event ->', e.type);
+  // console.assert.log('event ->', e.type);
 
-    e.preventDefault();
+  e.preventDefault();
 
+  // console.log('raw input →', inputEl.value);
+  // grab the data, clean and normalise
+  const userGuess = inputEl.value.trim().toLowerCase();
 
-    // console.log('raw input →', inputEl.value);
-    // grab the data, clean and normalise 
-    const userGuess = inputEl.value.trim().toLowerCase();
+  // console.log('clean guess →', `"${userGuess}"`);
 
-    // console.log('clean guess →', `"${userGuess}"`);
+  // make sure they enter something
+  // empty after the trim
+  if (userGuess === "") {
+    // console.log('empty guess – feedback shown');
+    feedbackEl.textContent = "Please enter a team name."; //message
+    feedbackEl.className = ""; //clear class
+    // console.log(userGuess)
 
-
-
-    // make sure they enter something 
-    // empty after the trim
-    if (userGuess === '') {
-
-        // console.log('empty guess – feedback shown');
-        feedbackEl.textContent = 'Please enter a team name.'; //message 
-        feedbackEl.className = ''; //clear class 
-        // console.log(userGuess)
-
-        return; //exit 
-    }
-    // console.log('checking answer for →', userGuess);
-    checkAnswer(userGuess);
+    return; //exit
+  }
+  // console.log('checking answer for →', userGuess);
+  checkAnswer(userGuess);
 }
 
-// Check the Answer 
-// i need to check to make sure the answers are correct 
+// Check the Answer
+// i need to check to make sure the answers are correct
 function checkAnswer(userGuess) {
+  // get correct for clue
+  const correct = teams[currentIndex].answer;
+  // console.log(' correct answer ->', correct);
 
-    // get correct for clue 
-    const correct = teams[currentIndex].answer;
-    // console.log(' correct answer ->', correct);
+  // compare userguess vs correct
+  if (userGuess === correct) {
+    score++; // increase score
+    // console.log('guess correct -> score =', score);
+    feedbackEl.textContent = "Correct"; //player sees message correct
+    feedbackEl.className = "correct";
+  } else {
+    console.log("guess wrong -> user:", userGuess, "| correct:", correct);
 
-    // compare userguess vs correct 
-    if (userGuess === correct) {
+    feedbackEl.textContent = `Wrong. Answer: ${correct}`; //player sees message wrong
+    feedbackEl.className = "wrong";
+  }
 
-        score++; // increase score
-        console.log('guess correct -> score =', score);
-        feedbackEl.textContent = 'Correct'; //player sees message correct
-        feedbackEl.className = 'correct';
-    } else {
-        console.log('guess wrong -> user:', userGuess, '| correct:', correct);
-
-        feedbackEl.textContent = `Wrong. Answer: ${correct}`; //player sees message wrong
-        feedbackEl.className = 'wrong';
-    }
-
-    // working so far !!!!!!!!!!!
+  // working so far !!!!!!!!!!!
 }
-// Update the score board 
+// Update the score board
 scoreEl.textContent = `Score: ${score}`;
-console.log('scoreboard updated ->', scoreEl.textContent);
+console.log("scoreboard updated ->", scoreEl.textContent);
 
-// Record this round - gotta make a function for the history 
+// Record this round - gotta make a function for the history
 addHistory(userGuess, correct);
 
-// go to next round 
+// go to next round
 currentIndex++;
-console.log('next index →', currentIndex);
-// if more clues remain -> increment currentIndex -> renderClue() again 
-// if no clues left -> endgame 
+console.log("next index -", currentIndex);
+// if more clues remain -> increment currentIndex -> renderClue() again
+// if no clues left -> endgame
 
-// if more clues left show the next clue and clear the field 
+// if more clues left show the next clue and clear the field
 if (currentIndex < teams.length) {
-    console.log('render next clue');
+  console.log("render next clue");
 
-    renderClue();
-    inputEl.value = '';
+  renderClue();
+  inputEl.value = "";
 } else {
-    console.log('no more clues – end game');
-    endGame();
+  console.log("no more clues - end game");
+  endGame();
 }
-// History Function - creating a new list item and append it 
+// History Function - creating a new list item and append it
 // see every attempt
-// build a node n insert append 
-// keep data seperate 
+// build a node n insert append
+// keep data seperate
 function addHistory(guess, answer) {
-    console.log('add to history -> guess:', guess, '| answer:', answer)
+  console.log("add to history -> guess:", guess, "| answer:", answer);
 
-    const li = document.createElement('li'); //make <li>
-    li.textContent = `You guessed "${guess}", answer was "${answer}"`; //fill text
-    historyEl.appendChild(li); // add to id="history"
-
+  const li = document.createElement("li"); //make <li>
+  li.textContent = `You guessed "${guess}", answer was "${answer}"`; //fill text
+  historyEl.appendChild(li); // add to id="history"
 }
 // END GAME FUNCTION
 function endGame() {
+  clueEl.textContent = "Game Over!"; //Replace clue with message
 
-    clueEl.textContent = 'Game Over!'; //Replace clue with message 
+  // finally score
+  // how many guesses were right vs how many clues was presented
+  // if score is 2 and teams.length is 3 final score: 2/3
+  //  i should see "Final Score: 2/3"
+  const finalMsg = `Final score: ${score} / ${teams.length}`;
+  feedbackEl.textContent = finalMsg;
+  feedbackEl.className = "correct";
+  console.log("Congrats", finalMsg);
 
-    // finally score 
-    // how many guesses were right vs how many clues was presented 
-    // if score is 2 and teams.length is 3 final score: 2/3
-    //  i should see "Final Score: 2/3"
-    const finalMsg = `Final score: ${score} / ${teams.length}`;
-    feedbackEl.textContent = finalMsg
-    feedbackEl.className = 'correct';
-console.log('Congrats', finalMsg)
-    
-// BOM method - pop up 
-    alert('Nice job finishing the NBA game!');
+  // BOM method - pop up
+  alert("Nice job finishing the NBA game!");
 }
 
-// Gotta reset the game 
+// Gotta reset the game
 function resetgame() {
+        currentIndex = 0; // 1st clue 
+
+    score = 0; // score 0
+
+    // Refresh everything from the previous game 
+    scoreEl.textContent = 'Score: 0'; //clear display 
+
+    feedbackEl.textContent = ''; //remove text 
+    feedbackEl.className = ''; // strip the colors class
+    inputEl.value = ''; //empty text box
+    historyEl.innerHTML = ''; //wipe history in li
+
+    renderClue(); //show 1st clue again 
 
 }
